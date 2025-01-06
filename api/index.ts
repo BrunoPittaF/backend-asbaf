7141089b4920819c46d9ff708c97b3587ad4eb3e
brunopittaf <caiobrunopittaf@gmail.com>
2025-01-06 12:49:03 -0300

    feat: change pastas

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,28 @@
-import app from '../src/index';
+import express from 'express';
+import userRoutes from './routes/user';
+import partnerRoutes from './routes/partner';
+
+
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/user', userRoutes);
+app.use('/partner', partnerRoutes);
+
+const port = process.env.PORT || 8080;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('API funcionando!');
+});
+
 
 export default app;
